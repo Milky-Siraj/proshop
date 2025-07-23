c47da83b517ada9dfa7d1dfaf11a1db71d4d8f1b
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2025-07-23 13:51:31 -0500

    custombootstrap theme

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png"; // Assuming you have a logo image
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img
+              src={logo}
+              alt="Proshop Logo"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            Proshop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
