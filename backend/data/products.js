8c31ef0d59686714ee4d56438ee3de4ff78dd6ed
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2025-07-30 06:50:59 -0500

    prepare sample data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
 const products = [
   {
-    _id: "1",
     name: "Airpods Wireless Bluetooth Headphones",
     image: "/images/airpods.jpg",
     description:
@@ -13,9 +12,20 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: "2",
-    name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
+    name: "mouse Wireless Bluetooth Headphones",
+    image: "/images/mouse.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "camera Wireless Bluetooth Headphones",
+    image: "/images/camera.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working.",
     brand: "Apple",
