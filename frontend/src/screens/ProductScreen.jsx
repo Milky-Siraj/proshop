33b5f33b906df14a74531313537e276d7344127a
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2025-08-02 11:39:17 -0500

    loader and message components

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
@@ -16,9 +18,11 @@ const ProductScreen = () => {
   return (
     <>
       {isLoading ? (
-        <h1> Loading....</h1>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           {" "}
